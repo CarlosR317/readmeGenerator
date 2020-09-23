84084bb350e66c8494f0f1f803eb6e41c80ccf30
CarlosR317 <cramire5@ycp.edu>
2020-09-23 14:50:35 -0400

    changes

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+const inquirer = require("inquirer");
+
+
+const generateMarkdown = require("./generateMarkdown");
+const fs = require("fs");
+
+fs.writeFile("");
+
 // array of questions for user
 const questions = [
-
+{name:"title",
+message:"StockFinder",
+type:"input",}
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function generateMarkdown(data) {
+    return '#${data.title}'
 }
+
+// description
 
 // function to initialize program
 function init() {
-
+inquirer.prompt(questions).then(function(answers){
+    generateMarkdown(data);
+})
 }
 
 // function call to initialize program
