6821e1db528b93d986478d130d201b642f8ea529
CarlosR317 <cramire5@ycp.edu>
2020-09-24 11:52:30 -0400

    additional changes to table of contents

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ const questions = [
         message: "what is your project about?",
         type: "input",
     }, {
+        // do I put the actual table of contents here?
         name: "table of contents",
         message: "",
-        type: "input",
+        // type: "input",
+    }, {
+        name: "installation",
+        message: "",
     }, {
         name: "usage",
         message: "What is this project used for?",
@@ -48,11 +52,12 @@ then(answers => { })
 // # Table of contents
 // 1. [Title](#title)
 // 2. [Description](#description)
-// 3. [Usage](#usage)
-// 4. [License](#license)
-// 5. [Contributing](#contributing)
-// 6. [Test](#tests)
-// 7. [Questions](#questions)
+// 3. [Installation](#installation)
+// 4. [Usage](#usage)
+// 5. [License](#license)
+// 6. [Contributing](#contributing)
+// 7. [Test](#tests)
+// 8. [Questions](#questions)
 
 // ## Title of the project <a name="title"></a>
 //do I type the title here or does it get placed here by the user input?
@@ -60,11 +65,15 @@ then(answers => { })
 // ## Description of the project <a name="description"></a>
 // do I type the description here or does it get placed here by the user input?
 
+// ## Installation <a name="installation"></a>
+// do I type the installation here or does it get placed here by the user input?
+
 // ### Usage for the project <a name="usage"></a>
 // do I type the usage here or does it get placed here by the user input?
 
 // ## License of project <a name="license"></a>
 // do I type the license here or does it get placed here by the user input?
+// "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
 
 // ## Contributions to project <a name="contributing"></a>
 // do I type the contributions here or does it get placed here by the user input?
@@ -82,8 +91,8 @@ function init() {
     inquirer.prompt(questions).then(function (answers) {
         var readMe = generateMarkdown(answers);
         fs.writeFile("generateReadme.md", function (error, questions) {
-            if (error) {return console.log(error)}
-             { console.log(questions) }
+            if (error) { return console.log(error) }
+            { console.log(questions) }
         })
     })
 }
