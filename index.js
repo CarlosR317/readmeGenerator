4b701055520e51e3fa4016d6e494e56d5e0eb3ed
CarlosR317 <cramire5@ycp.edu>
2020-09-24 11:40:30 -0400

    table of contents and license

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const questions = [
 
     }, {
         name: "license",
-
+        badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
     }, {
         name: "contributing",
         Message: "How did you contribute?",
@@ -38,7 +38,42 @@ const questions = [
         name: "questions",
     }
 ];
+
+// do i put this here? I thought the answers come form the user input?
 then(answers => { })
+
+
+// here is my table of contents. do I leave it here or do I put it in the array
+
+// # Table of contents
+// 1. [Title](#title)
+// 2. [Description](#description)
+// 3. [Usage](#usage)
+// 4. [License](#license)
+// 5. [Contributing](#contributing)
+// 6. [Test](#tests)
+// 7. [Questions](#questions)
+
+// ## Title of the project <a name="title"></a>
+//do I type the title here or does it get placed here by the user input?
+
+// ## Description of the project <a name="description"></a>
+// do I type the description here or does it get placed here by the user input?
+
+// ### Usage for the project <a name="usage"></a>
+// do I type the usage here or does it get placed here by the user input?
+
+// ## License of project <a name="license"></a>
+// do I type the license here or does it get placed here by the user input?
+
+// ## Contributions to project <a name="contributing"></a>
+// do I type the contributions here or does it get placed here by the user input?
+
+// ## Tests <a name="tests"></a>
+// do I type the tests here or does it get placed here by the user input?
+
+// ## Questions <a name="questions"></a>
+// do I type the questions here or does it get placed here by the user input?
 
 // description
 
@@ -46,12 +81,12 @@ then(answers => { })
 function init() {
     inquirer.prompt(questions).then(function (answers) {
         var readMe = generateMarkdown(answers);
-        fs.writeFile("generateReadme.md", function (error) {
+        fs.writeFile("generateReadme.md", function (error, questions) {
             if (error) {return console.log(error)}
              { console.log(questions) }
         })
     })
 }
 
-// function call to initialize program
+// function call to initialize program (do I need this one here?)
 init();
